91f39df782f760652d4416fd45e216f8f9276692
Welcome <ii>
2024-01-08 12:23:27 +0300

    Initial commit

---
+++ app/src/main/java/com/rohitanabhavane/chatgpt/LoginOrSignUpActivity.java
@@ -0,0 +1,42 @@
+package com.rohitanabhavane.chatgpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginOrSignUpActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_sign_up);
+
+        btnLogin = findViewById(R.id.btnMainLogin);
+        btnRegister = findViewById(R.id.btnMainSignUp);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrSignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginOrSignUpActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
